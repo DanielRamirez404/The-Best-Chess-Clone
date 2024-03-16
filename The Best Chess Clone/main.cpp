89aa076a1ebcc2c888cb25e8bfa766525dbe5845
DanielRamirez404 <danielramirezabou404@gmail.com>
2024-03-16 16:16:56 -0400

    Changed configurations to make possible to use SDL2

--- The Best Chess Clone/main.cpp
+++ The Best Chess Clone/main.cpp
@@ -1,7 +1,35 @@
+#include <SDL.h>
 #include <iostream>
+
+void run(SDL_Window* window);
 
 int main(int, char**)
 {
-	std::cout << "Hello Github!\n";
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+		return 1;
+
+	constexpr int windowSize{ 400 };
+	SDL_Window* window{ SDL_CreateWindow("Hello Github!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, windowSize, windowSize, SDL_WINDOW_SHOWN) };
+
+	if (!window)
+		return 1;
+
+	run(window);
+
+	SDL_DestroyWindow(window);
+
 	return 0;
+}
+
+void run(SDL_Window* window)
+{
+	SDL_Event event{};
+	while (true)
+	{
+		while (SDL_WaitEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+				return;
+		}
+	}
 }

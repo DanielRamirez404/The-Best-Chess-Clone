96cbb4c6975972f774daa7e10da3a944061c4d48
DanielRamirez404 <danielramirezabou404@gmail.com>
2024-03-22 22:09:33 -0400

    Created the Game class

--- The Best Chess Clone/main.cpp
+++ The Best Chess Clone/main.cpp
@@ -1,36 +1,14 @@
+#include "game.h"
 #include <SDL.h>
 #include <SDL_image.h>
 #include <iostream>
 
-void run(SDL_Window* window);
-
 int main(int, char**)
 {
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-		return 1;
-
-	constexpr int windowSize{ 400 };
-	SDL_Window* window{ SDL_CreateWindow("Hello Github!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, windowSize, windowSize, SDL_WINDOW_SHOWN) };
-
-	if (!window)
-		return 1;
-
-	run(window);
-
-	SDL_DestroyWindow(window);
-
+	constexpr int size{ 600 };
+	const Game game(size, "Chess");
+	if (game.hadError())
+		std::cout << game.getError() << '\n';
+	game.run();
 	return 0;
-}
-
-void run(SDL_Window* window)
-{
-	SDL_Event event{};
-	while (true)
-	{
-		while (SDL_WaitEvent(&event))
-		{
-			if (event.type == SDL_QUIT)
-				return;
-		}
-	}
 }
